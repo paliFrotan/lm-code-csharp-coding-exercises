127f95c83f832f7d7328c17cbdf5bb24ec0b4646
paliFrotan <99688802+paliFrotan@users.noreply.github.com>
2022-08-26 17:27:10 +0100

    Update Exercise001.cs
    this is answers except for counting linux users test

--- Exercises/Exercise001.cs
+++ Exercises/Exercise001.cs
@@ -8,35 +8,74 @@ namespace Exercises
     {
         public string CapitalizeWord(string word)
         {
-            // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(word))
+            {
+                return word;
+            }
+            char[] letters = word.ToCharArray();
+            letters[0] = char.ToUpper(letters[0]);
+            return new string(letters);
         }
 
         public string GenerateInitials(string firstName, string lastName)
         {
-            // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            char[] firstNameLetters = firstName.ToCharArray();
+            char[] lastNameLetters = lastName.ToCharArray();
+            firstNameLetters[0] = char.ToUpper(firstNameLetters[0]);
+            lastNameLetters[0] = char.ToUpper(lastNameLetters[0]);
+            return new string(firstNameLetters[0] + "." + lastNameLetters[0]);
         }
 
         public double AddVat(double originalPrice, double vatRate)
         {
-            // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            try
+            {
+                try
+                {
+                    double currentRate = (vatRate + 100) / 100;
+                    double result = (originalPrice * currentRate);
+                    return Math.Round(result, 2);
+                }
+                catch (Exception ex1) when (originalPrice < 0)
+                {
+                    string message = "Price cannot be negative. Please enter a valid price.";
+                    throw new ArgumentException(message, ex1);
+                }
+            }
+            catch (Exception ex) when (vatRate < 0)
+            {
+                string message = "VAT cannot be negative. Please enter a valid VAT.";
+                throw new ArgumentException(message, ex);
+            }
+            // commit not working?? from visual basic 
 
             // NB: Look in Exercise001Tests.cs
             //     There is a test with commented out assertions.
             //     For an extra challenge, uncomment those assertions and make that test pass too.
+
+
         }
+
 
         public string Reverse(string sentence)
         {
             // Replace the exception statement below with your code!
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+
+            if (string.IsNullOrEmpty(sentence))
+            {
+                return sentence;
+            }
+            char[] textArray = sentence.ToCharArray();
+            Array.Reverse(textArray);
+
+            string reversedText = new string(textArray);
+            return reversedText;
+
         }
 
         public int CountLinuxUsers(List<User> users)
         {
-            // Replace the exception statement below with your code!
             throw new NotImplementedException();
         }
     }
