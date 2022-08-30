a3f7be129e0a36ef6a80ef779bf225a81e6d38b7
Pali Frotan <pali.frotan@icloud.com>
2022-08-30 21:43:32 +0100

    updated just assert exception left

--- Exercises/Exercise003.cs
+++ Exercises/Exercise003.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Exercises
 {
@@ -7,19 +6,7 @@ namespace Exercises
     {
         
         public string[] IceCreamFlavours => new string[] {"Pistachio", "Raspberry Ripple", "Vanilla", "Mint Chocolate Chip", "Chocolate", "Mango Sorbet"};
-
-       
-        /*public int IceCreamCode(string iceCreamFlavour)
-        {
-            for (int i = 0; i < IceCreamFlavours.Length; i++)
-            {
-                if (IceCreamFlavours[i] == iceCreamFlavour)
-                {
-                    return i;
-                };
-            }
-            return -1; 
-        } */
+        
         public int IceCreamCode(string iceCreamFlavour)  => Array.IndexOf(IceCreamFlavours, iceCreamFlavour);
     }
 }
